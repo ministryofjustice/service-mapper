b1f721ad4625c81e0ced82194e2f673cd8dc2f6e
James Darling <james@abscond.org>
2014-04-23 16:38:51 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Archipelago::Application.config.secret_key_base = '30d94a2816a62ec468658a479b320a02be2c19d1b8801783dea6ea91c8e5508c13da2c7de8795960d1aac8568952fe9d71720a0835115b18b717f5b187e79495'
