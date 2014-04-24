6669b04a4c80829a011b37d0e7994d3556e6232b
James Darling <james@abscond.org>
2014-04-24 18:43:16 +0100

    Add story stage lines to graph

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -5,10 +5,11 @@ class GraphsController < ApplicationController
       format.json do 
         render :json => {
           :nodes => 
-            System.all.collect {|x| {:id => x.id, :name => x.name, :type => 'system'}} +
-            Person.all.collect {|x| {:id => x.id, :name => x.role, :type => 'person'}},
+            System.all.collect {|x| {:id => "System_#{x.id}", :name => x.name, :type => 'system'}} +
+            Person.all.collect {|x| {:id => "Person_#{x.id}", :name => x.role, :type => 'person'}},
           :links => 
-            SystemLink.all.collect {|x| {:source => x.system_a.id, :target => x.system_b.id}}
+            SystemLink.all.collect {|x| {:source => "System_#{x.system_a.id}", :target => "System_#{x.system_b.id}", :type => 'system_link'}} +
+            StoryStage.all.collect {|x| {:source => "#{x.from_type}_#{x.from_id}", :target => "#{x.to_type}_#{x.to_id}", :type => 'story_stage'}}
         }
       end
     end
