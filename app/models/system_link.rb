ac52d88b35f1f24e3bbdc9205884e72194453e2b
James Darling <james@abscond.org>
2014-04-24 10:57:01 +0100

    Fix system link relations

--- app/models/system_link.rb
+++ app/models/system_link.rb
@@ -1,4 +1,4 @@
 class SystemLink < ActiveRecord::Base
-  belongs_to :system_a
-  belongs_to :system_b
+  belongs_to :system_a, class_name: "System"
+  belongs_to :system_b, class_name: "System"
 end
