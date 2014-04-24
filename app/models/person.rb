482c13cae342424b213926cd18f8cefff9f4659c
James Darling <james@abscond.org>
2014-04-24 21:46:29 +0100

    forgot to add has_manys to person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
+  has_many :story_stage_entries, :class_name => 'StoryStage', :as => 'to'
+  has_many :story_stage_exits, :class_name => 'StoryStage', :as => 'from'
   def name
     role
   end
