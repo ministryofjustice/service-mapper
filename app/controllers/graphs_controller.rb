aa3c7060af016294c6e3f4562a4b250258c6a65e
James Darling <james@abscond.org>
2014-04-24 14:23:54 +0100

    Show people in graph

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,7 +2,15 @@ class GraphsController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json { render :json => {:nodes => System.all.collect {|x| {:id => x.id, :name => x.name, :group => 1}}, :links => SystemLink.all.collect {|x| {:source => x.system_a.id, :target => x.system_b.id}}}}
+      format.json do 
+        render :json => {
+          :nodes => 
+            System.all.collect {|x| {:id => x.id, :name => x.name, :type => 'system'}} +
+            Person.all.collect {|x| {:id => x.id, :name => x.role, :type => 'person'}},
+          :links => 
+            SystemLink.all.collect {|x| {:source => x.system_a.id, :target => x.system_b.id}}
+        }
+      end
     end
   end
 end
