e8eac857f94008a377356a7c785b806cd9c665f6
James Darling <james@abscond.org>
2014-04-25 11:18:33 +0100

    Add IDs to graph links
    Don’t actually have a reason to do this yet, but I’m sure it will come
    in handy :/

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -8,8 +8,8 @@ class GraphsController < ApplicationController
             System.all.collect {|x| {:id => "System_#{x.id}", :name => x.name, :type => 'system'}} +
             Person.all.collect {|x| {:id => "Person_#{x.id}", :name => x.role, :type => 'person'}},
           :links => 
-            SystemLink.all.collect {|x| {:source => "System_#{x.system_a.id}", :target => "System_#{x.system_b.id}", :type => 'system_link'}} +
-            StoryStage.all.collect {|x| {:source => "#{x.from_type}_#{x.from_id}", :target => "#{x.to_type}_#{x.to_id}", :type => 'story_stage'}}
+            SystemLink.all.collect {|x| {:source => "System_#{x.system_a.id}", :target => "System_#{x.system_b.id}", :type => 'system_link', :id => x.id}} +
+            StoryStage.all.collect {|x| {:source => "#{x.from_type}_#{x.from_id}", :target => "#{x.to_type}_#{x.to_id}", :type => 'story_stage', :id => x.id}}
         }
       end
     end
