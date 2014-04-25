8a7aaa17722af95a746297369cffcf9fb404e9a8
James Darling <james@abscond.org>
2014-04-25 11:54:29 +0100

    Group and order from and to select box on story stages

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,11 @@
 class Person < ActiveRecord::Base
   has_many :story_stage_entries, :class_name => 'StoryStage', :as => 'to'
   has_many :story_stage_exits, :class_name => 'StoryStage', :as => 'from'
+  scope :alphabetical, order("role ASC")
   def name
     role
+  end
+  def polymorphic_id
+    "#{self.class.name}_#{self.id}"
   end
 end
--- app/models/system.rb
+++ app/models/system.rb
@@ -2,4 +2,10 @@ class System < ActiveRecord::Base
   has_many :system_links
   has_many :story_stage_entries, :class_name => 'StoryStage', :as => 'to'
   has_many :story_stage_exits, :class_name => 'StoryStage', :as => 'from'
+
+  scope :alphabetical, order("name ASC")
+
+  def polymorphic_id
+    "#{self.class.name}_#{self.id}"
+  end
 end
