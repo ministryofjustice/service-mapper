8d3298fdad7336d59f0e95a2f9461a563f858743
James Darling <james@abscond.org>
2014-04-25 13:28:32 +0100

    remove option to switch story or service

--- app/controllers/story_stages_controller.rb
+++ app/controllers/story_stages_controller.rb
@@ -79,6 +79,6 @@ class StoryStagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_stage_params
-      params.require(:story_stage).permit(:story_id, :from_str, :to_str, :payload, :description)
+      params.require(:story_stage).permit(:from_str, :to_str, :payload, :description)
     end
 end
