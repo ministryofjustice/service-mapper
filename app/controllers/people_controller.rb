30b8002b58bcce2a624a1d29a478f0468d2fdd13
James Darling <james@abscond.org>
2014-05-01 11:54:30 +0100

    add location to people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -72,6 +72,6 @@ class PeopleController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def person_params
-      params.require(:person).permit(:name, :role, :description)
+      params.require(:person).permit(:name, :role, :description, :location)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501102737) do
+ActiveRecord::Schema.define(version: 20140501105019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140501102737) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "location"
   end
 
   create_table "services", force: true do |t|
