0fe4fc9f9fc471245a8fa84f1008aa80f3743bce
James Darling <james@abscond.org>
2014-05-06 17:33:55 +0100

    heroku smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Archipelago::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { :host => 'archipelago.dsd.io' }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
