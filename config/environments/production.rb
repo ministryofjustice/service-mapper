bd12c1293c88f6d86dfc6179ffc8997e8a64efa4
James Darling <james@abscond.org>
2014-05-21 11:04:27 +0100

    Set base url differently if staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,11 @@ Archipelago::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'archipelago.dsd.io' }
+  if ENV['STAGING']
+    config.action_mailer.default_url_options = {:host => 'archipelagostaging.herokuapp.com' }
+  else
+    config.action_mailer.default_url_options = {:host => 'archipelago.dsd.io' }
+  end
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
