ada3ee53e04ee63e1cb34474bbf6cdc4a3c6051d
James Darling <james@abscond.org>
2014-05-21 10:59:15 +0100

    Don't check permissions if in sandbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,19 +10,23 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_verified_user!
-    if current_user
-      unless ["editor", "verified"].include?(current_user.role)
-        render :text => "Your account has not been verified yet"
-        return false
+    unless ENV['SANDBOX']
+      if current_user
+        unless ["editor", "verified"].include?(current_user.role)
+          render :text => "Your account has not been verified yet"
+          return false
+        end
       end
     end
   end
 
   def authenticate_admin!
-    if current_user
-      unless current_user.role == "editor"
-        render :text => "You can only access this page if you are an admin"
-        return false
+    unless ENV['SANDBOX']
+      if current_user
+        unless current_user.role == "editor"
+          render :text => "You can only access this page if you are an admin"
+          return false
+        end
       end
     end
   end
