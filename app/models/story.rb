389752cc37570837b411dc90ad7606fc298ccaeb
James Darling <james@abscond.org>
2014-05-31 16:22:25 +0100

    dependent destroy the story links

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,8 +2,8 @@ class Story < ActiveRecord::Base
   belongs_to :service
   belongs_to :replaces_story, :class_name => "Story"
   has_many :story_stages, :dependent => :destroy
-  has_many :story_link_entrances, :class_name => "StoryLink", :foreign_key => "to_id"
-  has_many :story_link_exits, :class_name => "StoryLink", :foreign_key => "from_id"
+  has_many :story_link_entrances, :class_name => "StoryLink", :foreign_key => "to_id", :dependent => :destroy
+  has_many :story_link_exits, :class_name => "StoryLink", :foreign_key => "from_id", :dependent => :destroy
   has_many :parents, :through => :story_link_entrances, :source => 'from'
   has_many :children, :through => :story_link_exits, :source => 'to'
 
