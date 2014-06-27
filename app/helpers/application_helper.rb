1e72fcef25e1044cdd81ac8c3755202d9f914baf
James Darling <james@abscond.org>
2014-06-27 23:34:56 +0100

    Allow nil status in status_flag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def status_flag(status)
-    "<span class='status #{status.downcase}'>#{status.upcase}</span>".html_safe
+    "<span class='status #{status.downcase}'>#{status.upcase}</span>".html_safe if status.present?
   end
 end
